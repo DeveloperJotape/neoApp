378d8975b28d704fd1d2b731330ddc05bd593c83
João <developerjotape@gmail.com>
2025-09-02 09:40:11 -0300

    feat: cpf só aceita 11 dígitos

--- src/main/java/br/com/devjoaopedro/neo/dto/ClienteRequestDTO.java
+++ src/main/java/br/com/devjoaopedro/neo/dto/ClienteRequestDTO.java
@@ -5,11 +5,12 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record ClienteRequestDTO(
     @NotBlank
     String nome,
-    @NotBlank
+    @NotBlank @Pattern(regexp = "\\d{11}")
     String cpf,
     @NotBlank @Email
     String email,
