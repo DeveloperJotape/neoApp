c01aab8ab9c8a0d29662b4d0a4e3bc5742eb91e3
João <developerjotape@gmail.com>
2025-09-02 09:49:19 -0300

    fix: erro 400 agora retorna uma lista JSON com os campos inválidos

--- src/main/java/br/com/devjoaopedro/neo/exception/TratadorDeErros.java
+++ src/main/java/br/com/devjoaopedro/neo/exception/TratadorDeErros.java
@@ -2,6 +2,7 @@ package br.com.devjoaopedro.neo.exception;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -18,14 +19,13 @@ public class TratadorDeErros {
     // Erro 404
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> tratarErro404(EntityNotFoundException ex) {
-        return respostaErro(HttpStatus.NOT_FOUND, "Cliente não encontrado", ex.getMessage());
+        return respostaErro(HttpStatus.NOT_FOUND, "Recurso não encontrado", ex.getMessage());
     }
 
     // Erro 400
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> tratarErro400(MethodArgumentNotValidException ex) {
-        var erros = ex.getFieldErrors().stream()
-                .map(erro -> erro.getField() + ": " + erro.getDefaultMessage())
+    public ResponseEntity<Map<String, Object>> tratarErro400(MethodArgumentNotValidException ex) {
+        List<String> erros = ex.getFieldErrors().stream().map(erro -> erro.getField() + ": " + erro.getDefaultMessage())
                 .toList();
         return respostaErro(HttpStatus.BAD_REQUEST, "Erro de validação", erros.toString());
     }
@@ -36,14 +36,14 @@ public class TratadorDeErros {
         return respostaErro(HttpStatus.INTERNAL_SERVER_ERROR, "Erro interno", ex.getMessage());
     }
 
-    private ResponseEntity<Map<String, Object>> respostaErro(HttpStatus status, String erro, String detalhes) {
+    private ResponseEntity<Map<String, Object>> respostaErro(HttpStatus status, String erro, Object detalhes) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("status", status.value());
         body.put("erro", erro);
         body.put("detalhes", detalhes);
-        
+
         return ResponseEntity.status(status).body(body);
     }
-    
+
 }
