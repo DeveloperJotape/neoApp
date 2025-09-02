91dd6fd39d15973fbde8ff48c003ac96e30a851b
João <developerjotape@gmail.com>
2025-09-01 23:18:44 -0300

    feat: repository criado

---
+++ src/main/java/br/com/devjoaopedro/neo/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package br.com.devjoaopedro.neo.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.devjoaopedro.neo.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+    
+}
